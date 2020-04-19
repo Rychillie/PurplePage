2721f677dac23feb5369fff549fa4e98588d512c
Rychillie Umpierre de Oliveira <rychillie@hotmail.com>
2020-04-19 03:02:36 -0300

    header

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,37 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: 'PurplePage',
+    description: `Lading Page OpenSource feito pela comunidade`,
+    author: `@rychillie`,
+    siteUrl: `https://rychillie.net`,
+    titleTemplate: "%s · PurplePage",
+    url: "https://rychillie.net",
+    image: "/images/logo.png",
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Rychillie`,
+        description: 'Brazilian Front-End Developer',
+        short_name: `Rychillie`,
+        start_url: `/`,
+        background_color: `#FF5757`,
+        theme_color: `#1A1A1A`,
+        display: `standalone`,
+        icon: `${__dirname}/static/images/logo.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Nunito`,
+          `source sans-serif\:400,700` // you can also specify font weights and styles
+        ],
+        display: 'swap'
+      }
+    },
+    `gatsby-plugin-sass`,
+  ],
 }
