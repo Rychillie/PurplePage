21004f26a6cb2b4f904ad3fd307b4007f1ef92c8
Rychillie Umpierre de Oliveira <rychillie@hotmail.com>
2020-04-22 00:39:03 -0300

    box-download

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Header from "../components/header"
 import Footer from "../components/footer"
@@ -7,6 +8,13 @@ const IndexPage = () => {
     return (
         <div>
             <Header/>
+            <section className={"container"}>
+                <div className={"box"}>
+                    <h3>Start using Abstract app now!</h3>
+                    <p>Dictas scaevola democritum cu his, magna  abhorreant dissentias.</p>
+                    <Link to="/" className={"download"} title={"downloadapp"}>Download App ⟶</Link>
+                </div>
+            </section>
             <Footer/>
         </div>
     )
