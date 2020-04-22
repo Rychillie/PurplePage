b770bc640c7414004c5dbada25ece615abe2fe1c
Rychillie Umpierre de Oliveira <rychillie@hotmail.com>
2020-04-21 23:54:40 -0300

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,55 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import Logo from "../../static/images/logo-footer.png"
+import GooglePlay from "../../static/images/GooglePlay.png"
+import AppStore from "../../static/images/APPStore.png"
+
 const Footer = () => (
-    <footer>
-        <div className={"container"}>
-            <div className={"row"}>
-                <div className={"col-5"}>
-
-                </div>
-                <div className={"col-7"}>
-
-                </div>
+    <footer className={"container"}>
+        <div className={"row"}>
+            <div className={"col-5"}>
+                <Link to="/" title={"Purple"} className={"logo"}>
+                    <img src={Logo} alt={"logo"} />
+                </Link>
+                <Link to="/" title={"Purple"} className={"googleplay"}>
+                    <img src={GooglePlay} alt={"GooglePlay"} />
+                </Link>
+                <Link to="/" title={"Purple"} className={"appstore"}>
+                    <img src={AppStore} alt={"AppStore"} />
+                </Link>
+                <p className={"copy"}>2018 © Copyrights, All rights reserved.</p>
+            </div>
+            <div className={"col-7"}>
+                <ul className={"menu-footer"}>
+                    <li className={"lista"}>
+                        <ul className={"lista-footer"}>
+                            <li className={"item-lista"}>About us</li>
+                            <li className={"item-lista"}>Home</li>
+                            <li className={"item-lista"}>Features</li>
+                            <li className={"item-lista"}>Docs</li>
+                            <li className={"item-lista"}>Pricing</li>
+                        </ul>
+                    </li>
+                    <li className={"lista"}>
+                        <ul className={"lista-footer"}>
+                            <li className={"item-lista"}>Features</li>
+                            <li className={"item-lista"}>Services</li>
+                            <li className={"item-lista"}>Online Shop </li>
+                            <li className={"item-lista"}>Influences</li>
+                            <li className={"item-lista"}>Media</li>
+                            <li className={"item-lista"}>Brands</li>
+                        </ul>
+                    </li>
+                    <li className={"lista"}>
+                        <ul className={"lista-footer"}>
+                            <li className={"item-lista"}>Blog</li>
+                            <li className={"item-lista"}>Terms & conditions</li>
+                            <li className={"item-lista"}>Support</li>
+                            <li className={"item-lista"}>Privacy</li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
         </div>
     </footer>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 
 import Header from "../components/header"
+import Footer from "../components/footer"
 
 const IndexPage = () => {
     return (
         <div>
             <Header/>
+            <Footer/>
         </div>
     )
 }
