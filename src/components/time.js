75096a0942062699b603f9b986396d5345364474
Rychillie Umpierre de Oliveira <rychillie@hotmail.com>
2020-04-26 02:42:27 -0300

    save time

---
+++ src/components/time.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import TimePhone from "../../static/images/phone-time.svg"
+
+const Time = () => (
+    <section className={"container"}>
+        <div className={"time"}>
+            <div className={"left"}>
+                <img src={TimePhone} alt="Icone"/>
+            </div>
+            <div className={"right"}>
+                <h3>Save  your time</h3>
+                <p>Dictas scaevola democritum cu his, magna abhorreant dissentias utDictas scaevola democritum cu his.</p>
+                <Link to="/" className={"download"} title={"downloadapp"}>Download App ⟶</Link>
+            </div>
+        </div>
+    </section>
+)
+
+export default Time
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from "react"
 import Header from "../components/header"
 import CardsPrimary from "../components/cards-primary"
 import Design from "../components/design"
+import Time from "../components/time"
 import Pricing from "../components/pricing"
 import Box from "../components/box"
 import Footer from "../components/footer"
@@ -13,6 +14,7 @@ const IndexPage = () => {
             <Header/>
             <CardsPrimary/>
             <Design/>
+            <Time/>
             <Pricing/>
             <Box/>
             <Footer/>
